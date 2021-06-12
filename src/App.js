0ef2bec0f27fff10f6bf9781782a6bfc0de26855
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-12 23:03:26 +0700

    Add TodoList in React Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import ColorBox from "./components/ColorBox"
+import ColorBox from "./components/ColorBox";
+import Todolist from "./components/Todolist";
+import React, { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState(
+    [{ id: 1, title: 'I love Easy Frontend! ' },
+    { id: 2, title: 'We love Easy Frontend! ' },
+    { id: 3, title: 'They love Easy Frontend! ' }]
+  )
+  
+  function handleTodolist(todo){
+    const newArray = [...todoList];
+    setTodoList(newArray.filter(item => item.id !== todo.id))
+  }
+
   return (
     <div className="App">
-      <h1>Hello Worlds!</h1>
+      <h1>React hooks - TodoList!</h1>
 
-      <ColorBox />
+      <Todolist todos={todoList} onTodoClick={handleTodolist}/>
     </div>
   );
 }
