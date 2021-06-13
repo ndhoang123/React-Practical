dbed3d32fb2bbff5765cf9a62d78230d20199798
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-13 17:19:58 +0700

    [Hands-on React] Add Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+    const {color, text, onClick} = props;
+
+    return (
+        <div>
+            <button onClick={onClick} style={{ backgroundColor:color }} 
+            className='btn'>{text}
+            </button>
+        </div>
+    );
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+    text: 'add',
+};
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    handleOnClick: PropTypes.func,
+};
+
+export default Button;
